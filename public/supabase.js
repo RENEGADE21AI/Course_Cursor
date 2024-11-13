69649601a6a90b7d593a6ba75fd9201307af9508
RENEGADE21CODING <mjdel-bianco@students.wcpss.net>
2024-11-13 11:56:05 -0500

    Update supabase.js

--- public/supabase.js
+++ public/supabase.js
@@ -3,9 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 export default supabase;
